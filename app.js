75460227f35111f5fc4977e219a6cb19429a8b8f
solo-man1 <solomonraja761@gmail.com>
2026-06-11 12:31:45 +0530

    Updated the App

--- app.js
+++ app.js
@@ -36,7 +36,7 @@ app.use('/api/auth', authRoutes);
 // Error handler
 app.use(errorHandler);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 10000;
 const server = app.listen(PORT, () => {
   console.log(`Server Running on port ${PORT}`);
 });
